268e21f6fbf5bf4cb5c9ee7bbe13b5d6a24dfe5b
Gil <Gil@siera.local>
2017-05-17 14:43:27 -0400

    Initial Commit

---
+++ cprogramming/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cprogramming
+//
+//  Created by Gil on 5/17/17.
+//  Copyright © 2017 gilbertapiah. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
