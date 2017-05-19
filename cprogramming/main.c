e51fc1035b238690bbfb4271d3fbe9c8aa0b5dbc
Gil <Gil@siera.local>
2017-05-19 17:54:23 -0400

    apad

--- cprogramming/main.c
+++ cprogramming/main.c
@@ -2,14 +2,84 @@
 //  main.c
 //  cprogramming
 //
-//  Created by Gil on 5/17/17.
+//  Created by Gilbert Appiah on 5/19/17.
 //  Copyright © 2017 gilbertapiah. All rights reserved.
 //
 
+
+/*
+ *
+ * Write three functions that compute the sum of the numbers in a given an array of elements
+ *
+ *
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int sum(int size, int *arr);
+void userInput();
+void *append(int size, int *arr);
+
+int sum( int size, int *arr){
+    int summ =0;
+    size -=1;
+    while (size >=0 ) {
+       
+        summ =  summ+ arr[size];
+        size--;
+        
+    }
+    
+    return summ;
+}
+void *append(int size,int *arr){
+    int count=0, r;
+    srand((unsigned)time(NULL));
+    
+    for (count =0; count <= size ; count++) {
+        r = rand() %100;
+        arr[count] = r;
+    }
+  
+    return 0;
+}
+void userInput(){
+    int *arr = malloc(sizeof(int)), input = 0, size=0, count=0;
+    while (1) {
+        printf("0 to quit \n");
+        printf("1 fill array given size \n");
+        printf("2 to sum elements \n");
+        printf("Enter: \n");
+        scanf("%d", &input);
+        switch (input) {
+            case 0:
+                free(arr);
+                exit(1);
+            case 1:
+                printf("Enter size: \n");
+                scanf("%d", &size);
+                append(size, arr);
+                printf("\n[ ");
+                for (count =0; count< size; count++) {
+                    printf("%d ", arr[count]);
+                }
+                printf(" ]\n\n");
+                break;
+            case 2:
+                printf("\nSum:%d \n\n", sum(size, arr));
+                break;
+                
+            default:
+                break;
+        }
+    }
+
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    userInput();
     return 0;
+
 }
